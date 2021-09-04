2a304ff48cd4d01c6fe2b95d3c5df98074748817
Daniel Hookins <daniel@hookins.net>
2021-09-04 19:35:47 +0930

    Dockerise go api app

---
+++ Dockerfile
@@ -0,0 +1,31 @@
+# syntax=docker/dockerfile:1
+
+# Build
+
+FROM golang:1.16-alpine AS build
+
+WORKDIR /app
+
+COPY go.mod ./
+COPY go.sum ./
+
+RUN go mod download
+
+COPY *.go ./
+
+RUN go build -o /main
+
+# Deploy
+FROM alpine:3.14
+
+WORKDIR /
+
+COPY --from=build /api /api
+
+COPY --from=build /usr/local/go/ /usr/local/go/
+ 
+ENV PATH="/usr/local/go/bin:${PATH}"
+
+EXPOSE 8080
+
+ENTRYPOINT ["/api"]
--- api.go
+++ api.go
@@ -6,21 +6,28 @@ import (
 
 func main() {
 	router := gin.Default()
-	
+
+	router.GET("/", home)
 	router.GET("/ping/:name/:action", ping)
-	
+
 	router.Run(":8080")
+}
+
+func home(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"success": "OK",
+	})
 }
 
 func ping(c *gin.Context) {
 	name := c.Param("name")
 	action := c.Param("action")
-	message:= name + " is " + action
+	message := name + " is " + action
 
 	// c.String(http.StatusOK, message)
 	c.JSON(200, gin.H{
-		"name": name,
-		"action": action,
+		"name":    name,
+		"action":  action,
 		"message": message,
 	})
 }
